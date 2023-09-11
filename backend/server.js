e1e00d23e152b666fd1290a88d01481d0be27904
Subin Lebow <subinlebow@gmail.com>
2023-09-11 18:43:25 -0400

    reorganized folder structure

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
